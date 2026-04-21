be3ba614e2b086ed25fa73a2a9e80147c9f0a949
Sergey-Yachmennikov <otsmmm2013@gmail.com>
2026-04-21 20:46:45 +0300

    first commit

---
+++ src/main/java/com/yachmennikov/cache_presentation/CachePresentationApplication.java
@@ -0,0 +1,13 @@
+package com.yachmennikov.cache_presentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachePresentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachePresentationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yachmennikov/cache_presentation/CachePresentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yachmennikov.cache_presentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachePresentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
